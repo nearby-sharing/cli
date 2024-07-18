251f7642b9cf978c42adec5b56622470a45d4874
Lukas Kurz <lukas@shortdev.de>
2024-07-18 11:03:50 +0200

    `SocketBroker`

---
+++ windows/NearShare.Windows.SocketBroker/SocketBroker.cs
@@ -0,0 +1,25 @@
+﻿using System.Runtime.InteropServices;
+
+namespace NearShare.Windows;
+public static partial class SocketBroker
+{
+    [StructLayout(LayoutKind.Sequential)]
+    struct ServiceTriggerRetrieveSocketsResult
+    {
+        nint a;
+        nint b;
+        nint c;
+    }
+
+    [LibraryImport("sbservicetrigger.dll")]
+    private static partial nint ServiceTriggerEnumerateTransferredSockets([MarshalAs(UnmanagedType.LPWStr)] string serviceName, out nint data);
+
+    [LibraryImport("sbservicetrigger.dll")]
+    private static partial nint ServiceTriggerRetrieveSockets([MarshalAs(UnmanagedType.LPWStr)] string serviceName, nint socket, out nint data);
+
+    public static void FreeSockets()
+    {
+        ServiceTriggerEnumerateTransferredSockets("CDPSvc", out var socketEnumeration);
+        ServiceTriggerRetrieveSockets("CDPSvc", socketEnumeration + 20, out _);
+    }
+}
